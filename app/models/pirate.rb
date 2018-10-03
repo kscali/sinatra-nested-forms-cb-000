45fe24c2f12b89d82f0ceff2d952c5d827bb8447
Koy Saeteurn <kosatun@aol.com>
2018-10-03 17:47:20 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,16 +1,16 @@
 class Pirate
   attr_reader :name, :weight, :height
-  
+
   PIRATES = []
-  
+
   def initialize(args)
-    @name = params[:name]
-    @weight = params[:weight]
-    @height = params[:height]
-    PIRATES << self 
-  end 
-  
-  def self.all 
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
     PIRATES
-  end  
+  end
 end
